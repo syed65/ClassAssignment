fba099d8ec2c4f5cb3120d9466e89252d6cc4047
Syed Almamun <mamun2630@gmail.com>
2018-01-10 11:42:15 -0500

    This is my 5th commit

---
+++ PositiveAndNegativeNum/src/icterguru/java/coditionalflow/NegNumPosNum.java
@@ -0,0 +1,18 @@
+package icterguru.java.coditionalflow;
+
+public class NegNumPosNum {
+
+	public static void main(String[] args) {
+		int x = -1;
+		int y = 1;		
+ if(x>=0 && y<=0) {
+	 System.out.println("X is a positive veriable & Y is a Negative variable");
+	 
+ }
+ else {
+	 System.out.println("X is a Negative veriable & Y is a Positive variable");
+ }
+ 
+	}
+
+}
